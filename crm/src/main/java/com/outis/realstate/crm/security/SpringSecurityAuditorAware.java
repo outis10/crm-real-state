0c2505549a81b5387ff6d6231ae7953b7d948632
outis10 <outis10@gmail.com>
2025-02-26 13:40:16 -0800

    Initial version of crm generated by generator-jhipster@8.9.0

---
+++ crm/src/main/java/com/outis/realstate/crm/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,18 @@
+package com.outis.realstate.crm.security;
+
+import com.outis.realstate.crm.config.Constants;
+import java.util.Optional;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of {@link AuditorAware} based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of(SecurityUtils.getCurrentUserLogin().orElse(Constants.SYSTEM));
+    }
+}
---
+++ crm/src/main/java/com/outis/realstate/crm/web/filter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Request chain filters.
+ */
+package com.outis.realstate.crm.web.filter;
